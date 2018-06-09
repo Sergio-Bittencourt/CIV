3bffd4c120e2c1f46d72cf5442fdf28d699cb5ee
Sergio-Bittencourt <sergioaugusto67@gmail.com>
2018-06-08 23:44:05 -0300

    Finish im2blocks function

--- encodeJPEG.m
+++ encodeJPEG.m
@@ -5,10 +5,12 @@
 %   
 function encodeJPEG(imageFilename)
 
-% Reads image in imageFilename into a matrix.
+% Reads image in imageFilename into a matrix and read the images'
+% dimensions.
 imageMatrix = imread(imageFilename);
+[imageSize, ~] = size(imageMatrix);
 
 % Separates imageMatrix into a cell array of 8x8 blocks.
-[imageBlocks] = image2blocks(imageMatrix);
+[imageBlocks] = image2blocks(imageMatrix, imageSize);
 
 end
--- image2blocks.m
+++ image2blocks.m
@@ -4,10 +4,21 @@
 %
 %   [imBlocks] = (imMatrix, blockParam)
 %       imMatrix: Chosen image in integer matrix form
+%       imSize:   Image height or width
 %       N:        Parameter used to determine the size of the blocks
 %       imBlocks: Cell array that contains the image blocks
 % 
-function imBlocks = image2blocks(imMatrix, N)
+function imBlocks = image2blocks(imMatrix, imSize, N)
 
+% If N isn't passed to the function set its default value to 8.
+if nargin < 3
+    N = 8;
+end
 
+% Calculate the number of NxN blocks that can be created.
+numberOfBlocks = imSize./N;
+
+% Generate the blocks.
+imBlocks = mat2cell(imMatrix, repmat(N, numberOfBlocks,1), ...
+    repmat(N, numberOfBlocks,1)); 
 end
